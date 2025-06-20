e05d7253434e483ce5bb99ffbf507098988c0ec2
wen2ms <1819509111@qq.com>
2025-06-20 23:36:22 +0800

    Feat: serialize

--- test/my_test.cpp
+++ test/my_test.cpp
@@ -5,4 +5,18 @@
 void MyTest::test() {
     Person person;
 
+    person.set_id(2013221);
+    person.set_age(33);
+    person.set_name("Juicy");
+    person.set_sex("Female");
+
+    std::string output;
+
+    person.SerializeToString(&output);
+
+    Person other;
+
+    other.ParseFromString(output);
+
+    std::cout << other.id() << ", " << other.age() << ", " << other.name() << ", " << other.sex();
 }
